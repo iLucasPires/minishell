c2db93f961ec4628f4f00ba2cc04bf10feb91553
lucas pn <piresnogueiralucas@gmail.com>
2022-11-17 16:00:29 -0300

    trying get cmd

--- sources/shell/repl.c
+++ sources/shell/repl.c
@@ -1,5 +1,35 @@
 #include <minishell.h>
 
+
+char	*get_acess_cmd(const char *path, char **dirs)
+{
+	t_command	c;
+	int 		index;
+
+	index = 0;
+	while (dirs[index])
+	{
+		c.path = ft_strjoin(dirs[index], path);
+		if (!access(c.path, F_OK))
+			return (c.path);
+		free(c.path);
+		index++;
+	}
+	return (NULL);
+}
+
+char	*get_cmd_path(char *line)
+{
+	char *command;
+	char **dirs;
+
+	dirs = ft_split(getenv("PATH"), ':');
+	command = get_acess_cmd(ft_strjoin("/", line), dirs);
+	
+
+	return (command);
+}
+
 static void	error_readline(void *ptr)
 {
 	if (ptr == NULL)
@@ -17,6 +47,7 @@ void	read_eval_print_loop(t_repl *repl)
 		repl->line = readline(PROMPT);
 		error_readline(repl->line);
 		add_history(repl->line);
+		printf("path command: %s\n", get_cmd_path(repl->line));
 		free(repl->line);
 	}
 }
