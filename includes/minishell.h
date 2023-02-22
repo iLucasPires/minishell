0def90a4a0df59466f535c8fdf2e1a41ed00d585
Lucas Pires Nogueira <68610729+iLucasPires@users.noreply.github.com>
2023-02-22 15:31:10 -0300

    changing header order

--- includes/minishell.h
+++ includes/minishell.h
@@ -15,14 +15,14 @@
 
 // libraries
 # include <fcntl.h>
-# include <readline/history.h>
-# include <readline/readline.h>
 # include <signal.h>
 # include <stdio.h>
 # include <stdlib.h>
 # include <sys/stat.h>
 # include <sys/wait.h>
 # include <unistd.h>
+# include <readline/history.h>
+# include <readline/readline.h>
 
 // libraries local
 # include "../libft/includes/libft.h"
